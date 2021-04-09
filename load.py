92a5c2efb2fdc36a00605be6aaf41bc807319ad9
Dthen <65791582+Dthen@users.noreply.github.com>
2021-04-09 11:24:04 +0100

    Update load.py
    Config still broken for sure.

--- load.py
+++ load.py
@@ -3,20 +3,31 @@ import sys
 import re
 import ttk
 import time
-import Tkinter as tk
 import requests
 import os
 import errno
 import glob
 import StringIO
 
+
 from PIL import Image
 
+from config import config, appname, applongname, appversion
 
-from config import applongname, appversion
-import myNotebook as nb
-from config import config
 
+
+import logging
+plugin_name = os.path.basename(os.path.dirname(__file__))
+logger = logging.getLogger(f'{appname}.{plugin_name}')
+if not logger.hasHandlers():
+    level = logging.INFO 
+    logger.setLevel(level)
+    logger_channel = logging.StreamHandler()
+    logger_formatter = logging.Formatter(f'%(asctime)s - %(name)s - %(levelname)s - %(module)s:%(lineno)d:%(funcName)s: %(message)s')
+    logger_formatter.default_time_format = '%Y-%m-%d %H:%M:%S'
+    logger_formatter.default_msec_format = '%s.%03d'
+    logger_channel.setFormatter(logger_formatter)
+    logger.addHandler(logger_channel)
 
 
 this = sys.modules[__name__]
@@ -27,26 +38,36 @@ this.debug=False
 
 def debug(d):
 	if this.debug==True:
-		print '[Screenshot] '+str(d)
+		logger.info('[Screenshot] '+str(d))
 
-
+#Load EDMC-Screenshot 3307
 def plugin_start():
-	"""
-	Load Screenshot plugin into EDMC
-	"""
-	this.stm_loc = tk.StringVar(value=config.get("STM"))
-	this.png_loc = tk.StringVar(value=config.get("PNG"))
-	this.delete_org = tk.StringVar(value=config.get("DelOrg"))
-	this.mkdir = tk.StringVar(value=config.get("Mkdir"))
-	print("Screenshot loaded!")
+	SteamLoc typing.Optional[tk.StringVar] = None
+	PngLoc typing.Optional[tk.StringVar] = None
+	DelOrig typing.Optional[tk.BooleanVar] = None
+	MkDir  typing.Optional[tk.BooleanVar] = None
+	this.stm_loc = tk.StringVar(value=config.get("SteamLoc"))
+	this.png_loc = tk.StringVar(value=config.get("PngLoc"))
+	this.DelOrig = tk.BooleanVar(value=config.get("DelOrig"))
+	this.MkDir = tk.BooleanVar(value=config.get("MkDir"))
+	logger.info("Steamshot loaded!")
 	return "Steamshot"
 
-	
+import typing
+from typing import Optional
+import tkinter as tk
+from tkinter import ttk
 
-def plugin_prefs(parent,cmdr,is_beta):  
-	frame = nb.Frame(parent)
+
+
+
+
+def plugin_prefs(parent: nb.Notebook, cmd: isbeta: bool) -> Optional[tk.Frame]:  
+
 	frame.columnconfigure(1, weight=1)
-
+	global MkDir 
+	MkDir = tk.StringVar(value=config.get_string("MkDir")
+	frame = nb.Frame(Parent)
 	stm_label = nb.Label(frame, text="Screenshot Directory")
 	stm_label.grid(padx=10, row=10, sticky=tk.W)
 
@@ -59,8 +80,10 @@ def plugin_prefs(parent,cmdr,is_beta):
 	png_entry = nb.Entry(frame, textvariable=this.png_loc)
 	png_entry.grid(padx=10, row=12, column=1, sticky=tk.EW)
 
-	nb.Checkbutton(frame, text="Delete Original File", variable=this.delete_org).grid()
-	nb.Checkbutton(frame, text="Group files by system directory", variable=this.mkdir).grid()
+	global DelOrig
+	DelOrig= tk.BooleanVar(value=config.get_boolean("DelOrig")
+	nb.Checkbutton(frame, text="Delete Original File")
+	nb.Checkbutton(frame, text="Group files by system directory", variable=this.MkDir).grid()
 	
 	return frame
 
@@ -70,7 +93,7 @@ def plugin_app(parent):
 	this.status = tk.Label(parent, anchor=tk.W, text="Ready")
 	
 	
-	return (label, this.status)
+	return (this.label, this.status)
 
 # Log in
 
@@ -78,8 +101,8 @@ def plugin_app(parent):
 def prefs_changed():
 	config.set("STM", this.stm_loc.get())
 	config.set("PNG", this.png_loc.get())
-	config.set("DelOrg", this.delete_org.get())
-	config.set("Mkdir", this.mkdir.get())
+	config.set("DelOrig", this.DelOrig.get())
+	config.set("MkDir", this.mkdir.get())
 	
 	
 
@@ -89,7 +112,7 @@ def get_sq(entry):
 	system = entry['System']
 	body = entry['Body']
 	dir = tk.StringVar(value=config.get("PNG")).get()
-	mkdir = tk.StringVar(value=config.get("Mkdir")).get()
+	mkdir = tk.StringVar(value=config.get("MkDir")).get()
 	
 	if mkdir:
 		mask = dir+'/'+system+'/'+'/*'+system+'('+body+')_*.png'
@@ -124,7 +147,7 @@ def make_sure_path_exists(path):
 # Detect journal events
 def journal_entry(cmdr, system, station, entry):
 
-    if entry['event'] == 'Screenshot':
+	if entry['event'] == 'Screenshot':
 		this.status['text'] = 'processing...'	
 		## get the numeric component from the filename	
 		seq = get_sq(entry)
@@ -145,27 +168,27 @@ def journal_entry(cmdr, system, station, entry):
 		## Construct the new filename	
 		pngfile=entry['System']+"("+entry['Body']+")_"+seq+".png"
 		
-		mkdir = tk.StringVar(value=config.get("Mkdir")).get()
-		print mkdir
+		mkdir = tk.StringVar(value=config.get("MkDir")).get()
+		logger.info(mkdir)
 		
 		
 		original = tk.StringVar(value=config.get("STM")).get() + '\\'+ steamfile
 		newdir = tk.StringVar(value=config.get("PNG")).get() + '\\'+ system
 
 		if mkdir == "1":
-			print "With dir " + mkdir
+			logger.info("With dir " + mkdir)
 			make_sure_path_exists(newdir)
 			converted = newdir + '\\'+ pngfile
-			print converted
+			logger.info(converted)
 		else:	
-			print "Without dir " + mkdir
+			logger.info("Without dir " + mkdir)
 			converted = tk.StringVar(value=config.get("PNG")).get() + '\\'+ pngfile
-			print converted
+			logger.info(converted)
 			
 		im = Image.open(original)
-		im.save(converted,"PNG");
+		im.save(converted,"PNG")
 		
-		delete_original = tk.StringVar(value=config.get("DelOrg")).get()	
+		delete_original = tk.StringVar(value=config.get("DelOrig")).get()	
 		
 
 		if delete_original:
